2cae6afdc9e8602ab0363408a04cd50c63a1d87f
Shrikant Koltur <55858514+Atomic-man007@users.noreply.github.com>
2024-03-12 12:48:30 +0900

    Update openpose.py

--- api/openpose.py
+++ api/openpose.py
@@ -21,14 +21,18 @@ import utils
 # Openpose class object 
 @dataclass
 class Openpose:
+    # Optional parameters for the Openpose class
     model: Optional[str] = None
     device: Optional[str] = None
     output_path: Optional[str] = None
 
+    # String representation of the Openpose object
     def __str__(self) -> str:
         return f"Openpose (model={self.model}, device={self.device}, output_path={self.output_path})"
 
+    # Initialization method for the Openpose class
     def __post_init__(self):
+        # Initialize the ControlNet model and the StableDiffusionControlNetPipeline
         self.controlnet = ControlNetModel.from_pretrained("fusing/stable-diffusion-v1-5-controlnet-openpose", torch_dtype=torch.float16)
         self.pipeline = StableDiffusionControlNetPipeline.from_pretrained(
             self.model, controlnet=self.controlnet,
@@ -36,14 +40,14 @@ class Openpose:
             use_auth_token=utils.use_auth_token(),
         )
 
+        # Set up device and other configurations
         self.pipeline.to(self.device)
         self.pipeline.safety_checker = utils.no_safety_checker
         self._compatible_schedulers = self.pipeline.scheduler.compatibles
         self.scheduler_config = self.pipeline.scheduler.config
         self.compatible_schedulers = {scheduler.__name__: scheduler for scheduler in self._compatible_schedulers}
 
-    # def _set_scheduler(self, scheduler_name):
-    #     self.pipeline.scheduler = UniPCMultistepScheduler.from_config(self.pipeline.scheduler.config)
+    # Method to generate image using the Openpose class
     def generate_image(
         self, 
         prompt, 
@@ -54,23 +58,30 @@ class Openpose:
         seed, 
         num_images: int=1,
     ):
-
+        # Generate a random seed if not provided
         if seed == -1:
-            # generate random seed
             seed = random.randint(0, 999999)
+
+        # Set up generator based on the device
         if self.device == "mps":
             generator = torch.manual_seed(seed)
             num_images = 1
         else:
             generator = torch.Generator(device=self.device).manual_seed(seed)
+
+        # Initialize OpenposeDetector processor
         self.processor = OpenposeDetector.from_pretrained("lllyasviel/Annotators")
-        
+
+        # Configure scheduler and enable various pipeline features
         self.pipeline.scheduler = UniPCMultistepScheduler.from_config(self.pipeline.scheduler.config)
         logger.info(self.pipeline.scheduler)
         self.pipeline.enable_xformers_memory_efficient_attention()
-        # self.pipeline.enable_attention_slicing(1)
         self.pipeline.enable_model_cpu_offload()
+
+        # Detect pose using the OpenposeDetector
         detected_pose = self.processor(image)
+
+        # Generate images using the pipeline
         output_images = self.pipeline(
             image=detected_pose,
             prompt=prompt,
@@ -80,6 +91,8 @@ class Openpose:
             guidance_scale=guidance_scale,
             num_images_per_prompt=num_images,
         ).images
+
+        # Create metadata for the generated images
         metadata = {
             "prompt": prompt,
             "negative_prompt": negative_prompt,
@@ -91,15 +104,19 @@ class Openpose:
         _metadata = PngInfo()
         _metadata.add_text("openpose", metadata)
 
+        # Save generated images and metadata to the specified output path
         utils.save_to_local_controlnet(
             images=output_images,
-            poses= detected_pose,
+            poses=detected_pose,
             module="openpose",
-            current_datetime = datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
+            current_datetime=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"),
             metadata=metadata,
             output_path=self.output_path,
         )
 
+        # Clear CUDA memory and perform garbage collection
         torch.cuda.empty_cache()
         gc.collect()
+
+        # Return generated images and metadata
         return output_images, _metadata
